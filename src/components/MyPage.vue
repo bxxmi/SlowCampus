e303390b90f3be13c52600ed69fef1bb5dc60ebd
kimyerimweb <kimyerimweb@gmail.com>
2021-11-14 23:31:08 +0900

    feat: implement my page navigator

---
+++ src/components/MyPage.vue
@@ -0,0 +1,55 @@
+<template>
+  <nav class="my-page-nav">
+    <button
+      type="button"
+      @click="choiceAuth">
+      정보 수정
+    </button>
+    <button
+      type="button"
+      @click="choiceOrder">
+      주문 내역
+    </button>
+    <button
+      type="button"
+      @click="choiceAccount">
+      계좌
+    </button>
+  </nav>
+
+  <div class="my-page-display">
+    <MyAuth v-show="navChoice==='auth'" />
+    <MyOrder v-show="navChoice==='order'" />
+    <MyAccount v-show="navChoice==='account'" />
+  </div>
+</template>
+
+<script>
+import MyAuth from '~/components/MyAuth'
+import MyOrder from '~/components/MyOrder'
+import MyAccount from '~/components/MyAccount'
+
+export default {
+    components: {
+        MyAuth,
+        MyOrder,
+        MyAccount
+    },
+    data() {
+        return {
+            navChoice: 'auth'
+        }
+    },
+    methods: {
+        choiceAuth() {
+            this.navChoice = 'auth'
+        },
+        choiceOrder() {
+            this.navChoice = 'order'
+        },
+        async choiceAccount() {
+          this.navChoice = 'account'
+        }
+    }
+}
+</script>
