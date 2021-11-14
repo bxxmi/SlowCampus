340ba93083f96de22abfaf7e3becde17241902ec
kimyerimweb <kimyerimweb@gmail.com>
2021-11-14 23:28:30 +0900

    feat: add exception handling in actions and etc
    필요에 의해 state, getters,mutation을 추가했습니다.
    state의 message는 에러 메시지,
    getters의 myAccountsNa...는 사용자에 종속되는 은행이름, 계좌id, 계좌번호로 한번에 요청하는 곳이 있습니다.

--- src/store/account.js
+++ src/store/account.js
@@ -3,44 +3,86 @@ import axios from 'axios'
 export default {
   namespaced: true,
 
-  state: () => ({}),
-  getters: {},
-  mutations: {},
+  state: () => ({
+    bankListCanChoice: [],
+    accountListandBalance: {},
+    message:''
+  }),
+  getters: {
+    myAccountsNameIdNumberInfo(state) {
+      return state.accountListandBalance.accounts && state.accountListandBalance.accounts.reduce((acc,el) => {
+        return [...acc, {name:el.bankName, id:el.id, accountNumber:el.accountNumber}]
+      },[])
+    }, //&&조건을 넣어줘야 undefined에 reduce실행하고 있다는 에러 발생 안함
+    unconnectedBanksInfo(state) {
+      return state.bankListCanChoice && state.bankListCanChoice.filter(bank => !bank.disabled)
+    },
+  },
+  mutations: {
+    setState(state,payload) {
+      Object.keys(payload).forEach(key => {
+        state[key] = payload[key]
+      })
+    }
+  },
   actions: {
-    async checkBankListCanChoice(input) {
+    async checkBankListCanChoice({commit},input) {
       const { username, Authorization } = input
 
-      const data = await axios({
-        //선택 가능한 은행 목록 조회
-        url: 'https://asia-northeast3-heropy-api.cloudfunctions.net/api/account/banks',
-        method: 'get', // GET method
-        headers: {
-          'Content-Type': 'application/json', // "Content-Type": "application/json"
-          apikey: 'FcKdtJs202110',
-          username,
-          Authorization,
-        },
+      commit('setState',{
+        bankListCanChoice: [],
       })
 
-      console.log(data, 'checkBankListCanChoice')
+      try {
+        const { data } = await axios({
+          url: 'https://asia-northeast3-heropy-api.cloudfunctions.net/api/account/banks',
+          method: 'get',
+          headers: {
+            'Content-Type': 'application/json',
+            apikey: 'FcKdtJs202110',
+            username: 'team2',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InFEM2M0dnp3RXByS2hrOExtWU9GIiwiaWF0IjoxNjM2ODkwMjE1LCJleHAiOjE2MzY5NzY2MTUsImlzcyI6InRoZXNlY29uQGdtYWlsLmNvbSJ9.Z90KRCeEVzuS0KwFDkPeVpwSi0orCn1fe7-zCcqbpTc',
+          },
+        })
+
+        commit('setState',{
+          bankListCanChoice: data,
+        })
+      } catch(e) {
+        commit('setState',{
+          message: e.message
+        })
+      }
     },
 
-    async ckeckAccountListandBalance(input) {
+    async checkAccountListandBalance({commit},input) {
       const { username, Authorization } = input
 
-      const data = await axios({
-        //계좌 목록 및 잔액 조회
-        url: 'https://asia-northeast3-heropy-api.cloudfunctions.net/api/account',
-        method: 'get', // GET method
-        headers: {
-          'Content-Type': 'application/json', // "Content-Type": "application/json"
-          apikey: 'FcKdtJs202110',
-          username,
-          Authorization, // 발행된 액세스 토큰
-        },
+      commit('setState',{
+        accountListandBalance: {}
       })
 
-      console.log(data, 'ckeckAccountListandBalance')
+      try {
+        const { data } = await axios({
+          //계좌 목록 및 잔액 조회
+          url: 'https://asia-northeast3-heropy-api.cloudfunctions.net/api/account',
+          method: 'get', // GET method
+          headers: {
+            'Content-Type': 'application/json', // "Content-Type": "application/json"
+            apikey: 'FcKdtJs202110',
+            username: 'team2',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InFEM2M0dnp3RXByS2hrOExtWU9GIiwiaWF0IjoxNjM2ODkwMjE1LCJleHAiOjE2MzY5NzY2MTUsImlzcyI6InRoZXNlY29uQGdtYWlsLmNvbSJ9.Z90KRCeEVzuS0KwFDkPeVpwSi0orCn1fe7-zCcqbpTc', // 발행된 액세스 토큰
+          },
+        })
+
+        commit('setState',{
+          accountListandBalance: data
+        })
+      }catch (e){
+        commit('setState',{
+          message: e.message
+        })
+      }
     },
 
     async connectAccount(input) {
