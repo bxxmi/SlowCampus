e7671548f856716c884301472888a3432a3332b0
park young chan <trollering12312@gmail.com>
2021-11-15 19:30:34 +0900

    remove : delete MyAuth and make MyInfo

--- src/store/auth.js
+++ src/store/auth.js
@@ -2,13 +2,14 @@ export default {
     namespaced: true,
     state: {
         encryptKey : "1234",
-        logged_in : false,
-        logged_user : {},
         APIheaderObj : {
             "content-type": "application/json",
             "apikey": "FcKdtJs202110",
             "username": "team2"
-        }
+        },
+        //변화되는 내용
+        logged_in : false,
+        logged_user : {},
     },
     mutations: {
       setUser(state, obj){
@@ -17,8 +18,5 @@ export default {
       changeLogged (state, val) {
         state.logged_in = val
       }
-    },
-    actions: {
-        
     }
   }
