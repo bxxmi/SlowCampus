a72a4d38a1bacfc55e5dd899d5b7e99ccb4c8ce9
bxxmi <bmi6638@gmail.com>
2021-11-16 20:11:19 +0900

    design: serach bar design

--- src/components/products/SearchProduct.vue
+++ src/components/products/SearchProduct.vue
@@ -5,7 +5,9 @@
       type="text" 
       placeholder="보고싶은 강의를 찾아보세요 😋" 
       @keyup.enter="searchProduct()" />
-    <button @click="searchProduct">
+    <button
+      class="search-btn"
+      @click="searchProduct">
       <span class="material-icons">
         search
       </span>
@@ -38,7 +40,18 @@ div {
   align-items: center;
   margin: 50px;
   input {
-    width: 200px;
+    width: 300px;
+    padding: 15px 20px;
+    border-radius: 50px;
+    border: none;
+    background-color: #eaeaea;
+    font-weight: 700;
+  }
+  .search-btn {
+    border: none;
+    background:transparent;
+    cursor: pointer;
+    color:#0D6EFD;
   }
 }
 </style>
