04db805103f9331e3e6442d52cf309268906aa7e
kimyerimweb <kimyerimweb@gmail.com>
2021-11-16 22:33:37 +0900

    refactor: divide my page into three pages
    and register on router

---
+++ src/components/myPage/MyPageNav.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="my-page-nav">
+    <RouterLink to="/myinfo">
+      정보 수정
+    </RouterLink>
+    <RouterLink
+      to="/myorder"
+      @click="choiceOrder">
+      주문 내역
+    </RouterLink>
+    <RouterLink
+      to="/myaccount"
+      @click="choiceAccount">
+      계좌 확인
+    </RouterLink>
+  </nav>
+</template>
