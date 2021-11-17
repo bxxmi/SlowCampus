ea5dbc94e85f983c6871c907cfa6c1ae10df73d1
bxxmi <bmi6638@gmail.com>
2021-11-17 18:51:30 +0900

    refactor: delete rollback

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,7 +10,6 @@ import MyAccount from '~/components/myPage/MyAccount'
 import Order from '~/routes/Order'
 import AfterOrder from '~/routes/AfterOrder'
 import ShoppingCart from '~/routes/ShoppingCart'
-import authfunc from '../store/authfunc'
 
 export default createRouter({
   history: createWebHistory(),
@@ -33,13 +32,7 @@ export default createRouter({
     },
     {
       path: '/createProduct',
-      component: CreateProduct,
-      beforeEnter: (to, from, next) => {
-        if (authfunc.getCookie('accessToken')) {
-          alert('유효한 사용자가 아닙니다.')
-          next('/')
-        }
-      }
+      component: CreateProduct
     },
     {
       path: '/myinfo',
