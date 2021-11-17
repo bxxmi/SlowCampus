729048207a6e5f721ec91a7551e6efbcdc15b5ba
bxxmi <bmi6638@gmail.com>
2021-11-17 17:21:44 +0900

    design: modify scss block

--- src/components/myPage/DetailOrderModal.vue
+++ src/components/myPage/DetailOrderModal.vue
@@ -21,14 +21,14 @@
   position: fixed;
   z-index: 999;
   .overlay {
-  width: 100%;
-  height: 100%;
-  position: fixed;
-  left: 0;
-  top: 0;
-  opacity: 0.3;
-  background-color: black;
-  }
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    left: 0;
+    top: 0;
+    opacity: 0.3;
+    background-color: black;
+    }
 }
 
 .modal-card {
@@ -44,17 +44,17 @@
   z-index: 999;
   box-shadow: 0 1px 5px rgba(0, 0, 0, 0.2);
   .close-btn {
-  width: 30px;
-  height: 30px;
-  background-color: #ededed;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  top: 25px;
-  right: 30px;
-  border-radius: 5px;
-  cursor: pointer;
+    width: 30px;
+    height: 30px;
+    background-color: #ededed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 25px;
+    right: 30px;
+    border-radius: 5px;
+    cursor: pointer;
   .fa-times {
     color:#999ba6;
   }
